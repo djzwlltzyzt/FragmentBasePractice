e9ef320bf4d85691e4eb4dce1b481fe13440867e
djzwlltzyzt <djzwlltzyzt@gmail.com>
2017-09-06 11:11:43 +0800

    Initial commit

---
+++ app/src/main/java/com/example/fragmentbasepractice/NewContentActivity.java
@@ -0,0 +1,13 @@
+package com.example.fragmentbasepractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewContentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_content_activity);
+    }
+}
---
+++ app/src/main/java/com/example/fragmentbasepractice/NewsContentFragment.java
@@ -0,0 +1,20 @@
+package com.example.fragmentbasepractice;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class NewsContentFragment extends Fragment {
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.news_content_frag, container, false);
+        return view;
+    }
+}

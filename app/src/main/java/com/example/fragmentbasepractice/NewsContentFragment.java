208f329f25d9d34a573676e4cc7b03380c7e49b8
djzwlltzyzt <djzwlltzyzt@gmail.com>
2017-09-07 17:38:00 +0800

    news

--- app/src/main/java/com/example/fragmentbasepractice/NewsContentFragment.java
+++ app/src/main/java/com/example/fragmentbasepractice/NewsContentFragment.java
@@ -6,15 +6,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class NewsContentFragment extends Fragment {
     private View view;
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable
+            Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.news_content_frag, container, false);
         return view;
+    }
+
+    public void refresh(String newsTitle, String newsContent) {
+        View visibilityLayout = view.findViewById(R.id.visibility_layout);
+        visibilityLayout.setVisibility(View.VISIBLE);
+        TextView newsTitleText = (TextView) view.findViewById(R.id.news_title);
+        TextView newsContentText = (TextView) view.findViewById(R.id.news_content);
+        newsTitleText.setText(newsTitle);
+        newsContentText.setText(newsContent);
     }
 }
